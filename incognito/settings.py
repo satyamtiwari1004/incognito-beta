e4b68b27ed9c1d5803d04c6c105aac324c445225
satyamtiwari1004 <satyamtiwari1004@gmail.com>
2021-02-24 19:12:23 +0530

    Beta Version 0.2 DuplicateFolderDeleted

--- incognito/settings.py
+++ incognito/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 CKEDITOR_UPLOAD_PATH="uploads/"
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -125,6 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static')
 ]
