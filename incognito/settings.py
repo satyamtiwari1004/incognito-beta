b0e2200b425dfe11e219bff8b4952acc9cc90c69
satyamtiwari1004 <63970740+satyamtiwari1004@users.noreply.github.com>
2021-03-02 22:10:26 +0530

    Removed Credentials

--- incognito/settings.py
+++ incognito/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parents[1]
 # See https://docs.djangoproject.com/en/dev/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'a8s8ghhns5edzbo-$joih)b-f&9gp(+#=%uj**$(034f5xjkfo'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -140,8 +140,8 @@ EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 #EMAIL_HOST = 'localhost'
-EMAIL_HOST_USER = 'verifyemail.incognito@gmail.com'
-EMAIL_HOST_PASSWORD = 'ptxueoskejllkjad'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT =587
 DEFAULT_FROM_EMAIL="Team Incognito"
 #
